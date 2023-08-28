38778e9dc1642e00114fd3bae0799a3c8508672c
BagoesPantera <bagoespantera01@gmail.com>
2023-08-28 16:46:14 +0800

    feat: add win condition

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 // DOM
 const btnOption = document.querySelectorAll('.button-option')
+const popUp = document.querySelector('.popup')
+const message = document.getElementById('message')
 
+// variable
 // x turn first
 let xTurn = true
 
-console.log(btnOption);
+const winningPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+function winChecker() {
+    for(let i of winningPatterns) {
+        // console.log(i); // => [0, 3, 6]
+        let [ele1, ele2, ele3] = [
+            btnOption[i[0]].innerText, // 0
+            btnOption[i[1]].innerText, // 3
+            btnOption[i[2]].innerText, // 6
+        ]
+        
+        if (ele1 != '' && ele2 != '' && ele3 != '') {
+            if (ele1 == ele2 && ele2 == ele3) {
+                popUp.classList.remove('hide')
+                message.innerHTML = `&#x1F389; <br> ${ele1} Wins`;
+            }
+        }
+    }
+}
 
 btnOption.forEach((element) => {
     element.addEventListener('click', () => {
@@ -17,5 +47,6 @@ btnOption.forEach((element) => {
         } 
         // disable the element, so you cant click it again
         element.disabled=true
+        winChecker()
     })
 })
